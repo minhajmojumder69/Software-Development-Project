d64a260e07d936f4dcfce565951cdf7c298714c9
minhajmojumder69 <minhajmojumder735@gmail.com>
2026-09-15 22:04:24 +0600

    Basic JavaScript

---
+++ Module 5/test.js
@@ -0,0 +1,75 @@
+// var test = 10  // int variable
+// var test2 = 'Hello World' // string variable
+
+// var obj ={
+//     name: 'minhaj',
+// };
+
+// var num = '12.3';
+// var num2 = 23;
+
+// console.log(parseFloat(num) + num2);
+// console.log(test2);
+// console.log(test);
+// console.log(typeof test);
+// console.log(typeof test2);
+// console.log(typeof obj);
+
+
+// if-else conditions  //
+// var status = 'rain'
+// if(status=='rain'){
+//     console.log('Baire jawa jabe naaa..');
+// }
+// else if(status=='gotom'){
+//     console.log('ai gorome tw bahire jawaii jabe naa..');
+// }
+// else{
+//     console.log('jao bahire theke ghure asho..');
+// }
+
+
+// Object //
+
+// var person= {
+//     name : 'minhaj',
+//     age : 22,
+//     Uni : "diu" ,
+//     friends: {
+//         name : 'nafisa' ,
+//         age : 21,
+//         uni : 'fku',
+//     }
+// };
+
+// console.log(person.friends.age);
+// console.log(person.age);
+
+// Array //
+// var friends = ['hero','alom','boka','condro',['rahim','karim','karina'],{name : 'hasina'}];
+
+// // console.log(friends[4][1]);
+// console.log(friends.length);
+// friends.pop();              // remove from last 
+// friends.push('gias');       // adding at last index
+// friends.unshift('gias');    // adding into [0] index
+// friends.shift();            // remove from [0] index
+// console.log(friends);
+
+// for (var i = 0; i < friends.length; i++) {
+//     var element = friends[i];
+
+//     if(element == 'alom'){
+//         console.log('Yes');
+//     }
+//     else{
+//         console.log('No');
+//     }
+// }
+
+function sum(num ,inum ) {
+    total = num +inum ;
+    console.log(total);
+}
+
+sum(10,27); 
