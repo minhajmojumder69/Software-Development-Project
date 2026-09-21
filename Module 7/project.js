ff89f13d9f11172c5e38795d1da77fb98bd9872d
minhajmojumder69 <minhajmojumder735@gmail.com>
2026-09-22 01:33:48 +0600

    api project

---
+++ Module 7/project.js
@@ -0,0 +1,57 @@
+const allProduct = () => {
+  fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+    .then((res) => res.json())
+    .then((data) => displayProducts(data.drinks));
+};
+
+// const buttonsClick = document.getElementsByClassName("item-btn");
+// for (const button of buttonsClick) {
+//   button.addEventListener("click", function (event) {
+//     event.preventDefault();
+//     button.setAttribute("disabled", true);
+//   });
+// }
+
+const displayProducts = (products) => {
+  const productcontainer = document.getElementById("product-container");
+
+  products.forEach((product) => {
+    const div = document.createElement("div");
+    div.classList.add("items-card");
+    div.innerHTML = `
+        <img class="card-img" src="${product.strDrinkThumb}"/>
+        <h3>Name: ${product.strDrink}</h3>
+        <h4>Category: ${product.strCategory}</h4>
+        <h5>Instructions: ${product.strInstructions.slice(0, 100)}</h5>
+        <button onclick="handletocart(this,'${product.strDrinkThumb}','${product.strDrink}')" class= "item-btn">Add to cart</button>
+        <button class= "item-btn">Details</button>
+        
+        `;
+
+    productcontainer.appendChild(div);
+    console.log(product);
+  });
+};
+
+let count = 0;
+const handletocart = (buttonElement,img, name) => {
+  const cartconteiner = document.getElementById("cart-conteiner");
+  if (count > 6) {
+    alert("Can't select more then 7 items");
+    return;
+  }
+
+  buttonElement.setAttribute("disabled", true);
+
+  const div = document.createElement("div");
+  div.classList.add("carts-card");
+  div.innerHTML = `
+        <img class="cart-img" src="${img}"/>
+        <h3 class="name" >${name.slice(0, 10)}</h3>
+        `;
+  cartconteiner.appendChild(div);
+  count = count + 1;
+  document.getElementById("total").innerText = count;
+};
+
+allProduct();
